5ea754dfe3c35bf027089c07a13734663bf68bec
Charles Franceskinj <charlyf65+github@gmail.com>
2023-06-16 19:32:55 -0400

    Add project files.

---
+++ WindowsDisplaySettingsPlugin/Actions/HDRCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace Loupedeck.WindowsDisplaySettingsPlugin.Actions
+{
+    using System;
+
+    public class HDRCommand: PluginDynamicCommand
+    {
+        private HDRCommand()
+        //public HDRCommand()
+            : base(displayName: "Toggle HDR", description: "Toggle HDR on/off", groupName: "Display")
+        {
+        }
+
+        protected override void RunCommand(String actionParameter)
+        {
+            //this.Plugin.ClientApplication.
+        }
+        // var colorInfo = new DISPLAYCONFIG_GET_ADVANCED_COLOR_INFO();
+
+        protected override String GetCommandDisplayName(String actionParameter, PluginImageSize imageSize) => $"HDR: {System.Windows.Forms.Screen.PrimaryScreen.DeviceName}";
+    }
+}
---
+++ WindowsDisplaySettingsPlugin/Actions/ProjectCommand.cs
@@ -0,0 +1,49 @@
+﻿namespace Loupedeck.WindowsDisplaySettingsPlugin.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using WindowsDisplayAPI.DisplayConfig;
+    using WindowsDisplayAPI.Native.DisplayConfig;
+
+    public class ProjectCommand: PluginDynamicCommand
+    {
+        private readonly Dictionary<String, DisplayConfigTopologyId> projectOptions = new Dictionary<String, DisplayConfigTopologyId>()
+        {
+            { "PC only", DisplayConfigTopologyId.Internal },
+            { "TV only", DisplayConfigTopologyId.External },
+            { "Duplicate", DisplayConfigTopologyId.Clone },
+            { "Extend", DisplayConfigTopologyId.Extend }
+        };
+
+        //private DisplayConfigTopologyId currentState;
+
+        public ProjectCommand(): base() 
+        {
+            //this.currentState = PathInfo.GetCurrentTopology();
+            foreach (var i in this.projectOptions)
+            {
+                this.AddParameter(i.Key, i.Key, "Display");
+            }
+        }
+
+        protected override void RunCommand(String actionParameter)
+        {
+            var topology = this.projectOptions[actionParameter];
+
+            try
+            {
+                PathInfo.ApplyTopology(topology, true);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            //finally 
+            //{
+            //    this.currentState = PathInfo.GetCurrentTopology();
+            //}
+                
+            
+        }
+    }
+}
